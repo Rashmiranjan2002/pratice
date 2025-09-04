882ba6a5a8fd89ea7e36d38bc3b79cff45b1837a
Rashmiranjan2002 <147078988+Rashmiranjan2002@users.noreply.github.com>
2025-09-05 01:48:17 +0530

    Update PraticeProjectApplicationTests.java

--- src/test/java/com/pratice_project/PraticeProjectApplicationTests.java
+++ src/test/java/com/pratice_project/PraticeProjectApplicationTests.java
@@ -2,9 +2,10 @@ package com.pratice_project;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-
+@ActiveProfiles("test")
 class PraticeProjectApplicationTests {
 
 	@Test
